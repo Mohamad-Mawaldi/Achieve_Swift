ccecc5c01c465123c5c156894e282a9f969f5e11
Mohamad Mawaldi <ima.mawaldi@gmail.com>
2020-10-06 19:51:06 +0200

    Added a signUp function

--- Achieve-iOS/ViewControllers/SignUpViewController.swift
+++ Achieve-iOS/ViewControllers/SignUpViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class SignUpViewController: UIViewController {
     
@@ -13,6 +15,8 @@ class SignUpViewController: UIViewController {
     @IBOutlet var emailTF: UITextField!
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
+    
+    var uid :String = "";
     
 
 
@@ -24,8 +28,21 @@ class SignUpViewController: UIViewController {
     
     
     @IBAction func SignUpClick(_ sender: Any) {
-        
+        if emailTF.text != nil && passwordTF.text != nil && userNameTF.text != nil{
+            Auth.auth().createUser(withEmail: emailTF.text!, password: passwordTF.text!, completion: { (result, error) in
+                if error != nil {
+                    print("there was an error", error!)
+                } else {
+                    self.uid = (result?.user.uid)!
+                    let ref = Database.database().reference(withPath: "users").child(self.uid).child("userInfo")
+                    ref.setValue(["email" : self.emailTF.text , "userName": self.userNameTF.text , "password" : self.passwordTF.text]  )
+                    
+                }
+            } )
+            
+        }
     }
+    
     @IBAction func dissMissClick(_ sender: Any) {
         
         dismiss(animated: true, completion: nil)
